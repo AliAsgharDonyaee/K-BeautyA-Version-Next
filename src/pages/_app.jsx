08dfff8324325c71d134953834250fc23d2a58dc
AliAsgharDevF <aliasghardevf@gmail.com>
2023-07-02 10:16:32 -0700

    add icon browser

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -10,8 +10,12 @@ const MyApp = ({ Component, pageProps }) => {
 				<link rel='preconnect' href='https://fonts.googleapis.com' />
 				<link rel='preconnect' href='https://fonts.gstatic.com' crossorigin />
 				<link href='https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap' rel='stylesheet' />
+				<link rel='icon' type='image/png' href='../../public/images/logo/next.svg' />
 				<meta name='description' content='Ask us for all brands of cosmetics' />
-				{/* KeywordS: wave, Womam, Water, Wind, Rose, Nature, Flower, Pure, Fresh, moisture, Texture, Soft */}
+				<meta
+					name='keywords'
+					content='KeywordS: wave, Womam, Water, Wind, Rose, Nature, Flower, Pure, Fresh, moisture, Texture, Soft'
+				/>
 				<meta name='viewport' content='width=device-width, initial-scale=1.0' />
 			</Head>
 			<Layouts>
